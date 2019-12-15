bed0bc98e083fa46f5432e6ece2abbb574c35d87
THCqingchen <45170773+THCqingchen@users.noreply.github.com>
2019-12-15 22:54:15 +0800

    Add files via upload

---
+++ PaginationConstant.java
@@ -0,0 +1,10 @@
+package com.itany.zshop.common.constant;
+
+public interface PaginationConstant {
+
+    //当前页，默认为1
+    public static final  int PAGE_NUM=1;
+    //每页显示条数，默认5条
+    public static final  int PAGE_SIZE=5;
+    public static final  int FRONT_PAGE_SIZE=8;
+}
